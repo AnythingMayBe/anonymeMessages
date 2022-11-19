0f21d3f8af5c2a3c78fde728ca89dc2db9e4c913
AnythingMayBe <anythingmaybe@protonmail.com>
2022-11-19 18:14:30 +0100

    Added errorhandler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import requests
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 webhookUrl = "" # The webhook URL that will get trigerred.
@@ -12,5 +13,12 @@ def index():
 def post():
     req = requests.post(webhookUrl, data={"content": request.form["message"]})
     return redirect("/")
+
+# Error Handler
+@app.errorhandler(Exception)
+def exception(error):
+    if isinstance(error, HTTPException):
+        return render_template("index.html"), 418
+    return render_template("index.html"), 500
 
 app.run()
