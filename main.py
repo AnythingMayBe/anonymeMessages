9efbb6483ccc105ab7383965ed1c4d70b3d229ba
AnythingMayBe <anythingmaybe@protonmail.com>
2022-11-19 18:05:50 +0100

    Added Discord webhook request

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
+webhookUrl = "" # The webhook URL that will get trigerred.
 
 @app.route("/")
 def index():
-    pass
+    return render_template("index.html")
 
-@app.route("/post")
+@app.route("/post", methods=["POST"])
 def post():
-    pass
+    req = requests.post(webhookUrl, data={"content": request.form["message"]})
+    return redirect("/")
 
 app.run()
